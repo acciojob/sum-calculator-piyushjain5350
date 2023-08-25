3c65eb6577b18ef695b4ccda78cd21ecde0a64dd
Piyush Jain <piyushjain5350@gmail.com>
2023-08-25 13:47:12 +0530

    1 soln

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
-
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [add,setAdd]=useState(0);
+  function additionFn(){
+    setAdd(()=>{
+      return Number(add)+Number(event.target.value);
+    })
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <input type="number" placeholder="0"  onChange={additionFn}/>
+
+        <p>{add}</p>
     </div>
   )
 }
